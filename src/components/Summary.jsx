718c56d3f881f6cd238f7e06f1f41cf94908f21d
masud-rana44 <mdmasudrana4488@gmail.com>
2023-07-09 03:16:27 +0600

    added dist

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,7 +3,7 @@ import useFetch from "../hooks/useFetch";
 import classes from "../styles/Summary.module.css";
 
 const Summary = ({ score, noq }) => {
-  const { result, loading, error } = useFetch(
+  const { result, loading } = useFetch(
     "https://api.pexels.com/v1/search?query=nature&per_page=1",
     {
       Authorization: "G5lutisdtcodIfI92X4TRJN5EjPwbC0vVCobhqiAXH3yd6ylff73h1A1",
@@ -24,12 +24,8 @@ const Summary = ({ score, noq }) => {
       </div>
 
       <div className={classes.badge}>
-        {error ? (
-          <div className="msg">{error}</div>
-        ) : (
-          loading && <div className="msg">Loading to display batch...</div>
-        )}
-        {!loading && !error && <img src={image} alt="image" />}
+        {loading && <div className="msg">Loading to display batch...</div>}
+        {!loading && <img src={image} alt="image" />}
       </div>
     </div>
   );
